7d21bcb4a6a877129e5f183324ec151c226f9bbe
squidawg <78506807+squidawg@users.noreply.github.com>
2023-06-14 19:01:28 +0400

    feat: implement 'File system' functions

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import fs  from 'fs'
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files';
+    const oldFileName = path.join(filePath, 'wrongFilename.txt');
+    const newFileName = path.join(filePath, 'properFilename.md');
+
+    if(!fs.existsSync(oldFileName) || fs.existsSync(newFileName)){
+        throw Error('FS operation failed');
+
+    }
+    fs.renameSync(oldFileName, newFileName);
 };
 
 await rename();
