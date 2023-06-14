a08b1c950c88fa567323e89d35349772e75b5492
squidawg <78506807+squidawg@users.noreply.github.com>
2023-06-14 18:53:43 +0400

    feat: implement "CLI" functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const result = args
+        .map( (arg) => arg
+            .startsWith('--') ?
+            arg.replace('--', ''): `is ${arg},`);
+
+    const resData = result
+        .join(' ')
+        .split(',')
+    console.log(resData
+        .slice(0 , resData.length - 1))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVar = process.env;
+    const resData = Object.keys(envVar)
+        .filter( param => param.includes('RSS_'))
+        .map( key => `${key}=${envVar[key]}`)
+    console.log(resData.join('; '));
 };
 
 parseEnv();
