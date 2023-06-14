2bcf526d4f323748a699182d012d06ee80a24a3b
squidawg <78506807+squidawg@users.noreply.github.com>
2023-06-14 22:04:07 +0400

    feat: implement "Zlib" functions

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import zlib from 'zlib'
+import fs from "fs";
+
+
 const decompress = async () => {
-    // Write your code here 
+
+    const outPath = 'src/zip/files/fileToCompress.txt';
+    const filePath = 'src/zip/files/archive.gz';
+
+    const unZip = zlib.createUnzip();
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(outPath);
+
+    readStream.pipe(unZip).pipe(writeStream);
 };
 
 await decompress();
